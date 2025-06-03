67dcf95cf3bec55109096d41bad9c8c1bc0cf473
out_iljin.sa <out_iljin.sa@pecom.ru>
2025-06-03 16:33:39 +0300

    CRUD

---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+	router.HandleFunc("GET /v1/movies", app.listMoviesHandler)
+	router.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	router.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
+	router.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
+	router.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
+
+	return router
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+migrate create -seq -ext sql -dir ./migrations create_movies_table
+migrate create -seq -ext sql -dir ./migrations add_movies_check_constraints
+migrate create -seq -ext sql -dir ./migrations add_movies_indexes
+migrate -path ./migrations -database='postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable' up
+
+
