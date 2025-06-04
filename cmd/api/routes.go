3e6b373c77869b8730b829ef98c17789d3f40815
out_iljin.sa <out_iljin.sa@pecom.ru>
2025-06-04 12:02:36 +0300

    Logger

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,12 +5,12 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
-	router.HandleFunc("GET /v1/movies", app.listMoviesHandler)
-	router.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	router.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
-	router.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
-	router.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
+	router.Handle("GET /v1/healthcheck", app.recoverPanic(http.HandlerFunc(app.healthcheckHandler)))
+	router.Handle("GET /v1/movies", app.recoverPanic(http.HandlerFunc(app.listMoviesHandler)))
+	router.Handle("POST /v1/movies", app.recoverPanic(http.HandlerFunc(app.createMovieHandler)))
+	router.Handle("GET /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.showMovieHandler)))
+	router.Handle("PATCH /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.updateMovieHandler)))
+	router.Handle("DELETE /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.deleteMovieHandler)))
 
 	return router
 }
