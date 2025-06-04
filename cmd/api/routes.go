0f59f087f12f5de0c4e0884e517dde32bd52912e
out_iljin.sa <out_iljin.sa@pecom.ru>
2025-06-04 13:03:57 +0300

    Server

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,12 +5,12 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	router := http.NewServeMux()
 
-	router.Handle("GET /v1/healthcheck", app.recoverPanic(http.HandlerFunc(app.healthcheckHandler)))
-	router.Handle("GET /v1/movies", app.recoverPanic(http.HandlerFunc(app.listMoviesHandler)))
-	router.Handle("POST /v1/movies", app.recoverPanic(http.HandlerFunc(app.createMovieHandler)))
-	router.Handle("GET /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.showMovieHandler)))
-	router.Handle("PATCH /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.updateMovieHandler)))
-	router.Handle("DELETE /v1/movies/{id}", app.recoverPanic(http.HandlerFunc(app.deleteMovieHandler)))
+	router.Handle("GET /v1/healthcheck", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.healthcheckHandler))))
+	router.Handle("GET /v1/movies", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.listMoviesHandler))))
+	router.Handle("POST /v1/movies", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.createMovieHandler))))
+	router.Handle("GET /v1/movies/{id}", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.showMovieHandler))))
+	router.Handle("PATCH /v1/movies/{id}", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.updateMovieHandler))))
+	router.Handle("DELETE /v1/movies/{id}", app.recoverPanic(app.rateLimit(http.HandlerFunc(app.deleteMovieHandler))))
 
 	return router
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,11 @@
 module greenlight
 
-go 1.23
+go 1.23.0
+
+toolchain go1.23.4
 
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )
