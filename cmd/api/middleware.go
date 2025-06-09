2523b8aebce4c72f2933eb01254d2d03887f3ede
out_iljin.sa <out_iljin.sa@pecom.ru>
2025-06-09 12:02:15 +0300

    Rate limiter improvements

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -10,7 +10,6 @@ import (
 	"net/http"
 	"strings"
 	"sync"
-	"time"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -27,58 +26,31 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	type client struct {
-		limiter  *rate.Limiter
-		lastSeen time.Time
-	}
-
 	var (
 		mu      sync.Mutex
-		clients = make(map[string]*client)
+		clients = make(map[string]*rate.Limiter)
 	)
 
-	go func() {
-		for {
-			time.Sleep(time.Minute)
-
-			mu.Lock()
-
-			for ip, client := range clients {
-				if time.Since(client.lastSeen) > 3*time.Minute {
-					delete(clients, ip)
-				}
-
-				mu.Unlock()
-			}
-		}
-	}()
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
-
-			mu.Lock()
-
-			if _, found := clients[ip]; !found {
-				clients[ip] = &client{
-					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
-				}
-			}
-
-			clients[ip].lastSeen = time.Now()
-			if !clients[ip].limiter.Allow() {
-				mu.Unlock()
-				app.rateLimitExceededResponse(w, r)
-				return
-			}
-
-			mu.Unlock()
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
 		}
 
+		mu.Lock()
+
+		if _, found := clients[ip]; !found {
+			clients[ip] = rate.NewLimiter(2, 4)
+		}
+
+		if !clients[ip].Allow() {
+			mu.Unlock()
+			app.rateLimitExceededResponse(w, r)
+			return
+		}
+
+		mu.Unlock()
 		next.ServeHTTP(w, r)
 	})
 }
